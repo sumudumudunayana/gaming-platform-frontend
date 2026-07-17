65b3a9bf1e78aaa174be53f39203e9e700fb1866
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-07-17 18:32:04 +0530

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import Navbar from '@/components/layout/Navbar';
 import HeroSection from '@/components/home/HeroSection';
+import FeaturesSection from '@/components/home/FeaturesSection';
 
 export default function Home() {
   return (
     <>
       <Navbar />
+
       <main>
         <HeroSection />
+        <FeaturesSection />
       </main>
     </>
   );
